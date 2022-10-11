5f4c9cab99c54aae59ff58c79efcdeb3adfc8e2e
webbyrasika <rvtembhare2605@gmail.com>
2022-10-11 12:07:46 +0530

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,173 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="text-center">
+    <!-- <h2>Cat Fats</h2> -->
+
+    <div
+      class="p-4 mt-6 mb-4 text-lg text-blue-700 bg-blue-200 rounded-base dark:bg-blue-200 dark:text-blue-800"
+    >
+      <span class="font-medium"> {{ result.fact }}</span>
+      <!-- <img
+          src="https://i.pinimg.com/736x/6b/79/df/6b79df7e9cf368f4ff22ddb50b45b819.jpg"
+          alt=""
+          class="max-w-xs h-auto"
+        /> -->
+    </div>
   </div>
+
+  <div class="inline-flex">
+    <button
+      @click="preLoad"
+      class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+    >
+      Prev
+    </button>
+    <button
+      @click="loadMore"
+      class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+    >
+      Next
+    </button>
+
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      <router-link to="/"> Logout</router-link>
+    </button>
+  </div>
+
+  <div class="flex justify-center">
+    <div class="mb-3 xl:w-96">
+      <div
+        class="input-group relative flex flex-wrap items-stretch w-full mb-4"
+      >
+        <input
+          type="search"
+          v-model="search"
+          class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          placeholder="Search Cat Breed Name"
+          aria-label="Search"
+          @keypress="catFactSearch"
+        />
+        <ul
+          v-if="search"
+          class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        >
+          <li
+            v-for="item in fetchEntry()"
+            :key="item.breed"
+            class="p-6 text-center flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <img
+              class="object-contain h-32 w-80"
+              src="https://as2.ftcdn.net/v2/jpg/03/35/68/15/1000_F_335681545_kbojstRCD25Ld9eXgQ8KQbJoQv62x89Y.jpg"
+              alt=""
+            />
+            <p class="font-bold">{{ item.breed }}</p>
+            {{ item.country }}, {{ item.coat }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="flex justify-center"
+    v-for="item in fetchEntry()"
+    :key="item.breed"
+  >
+    <ul class="bg-white rounded-lg border border-gray-400 w-96 text-gray-900">
+      <li class="px-6 py-2 border-b border-gray-300 w-full rounded-t-lg">
+        {{ item.breed }}
+      </li>
+    </ul>
+  </div>
+
+  <!-- <div>
+    <div
+      v-for="item in fetchEntry()"
+      :key="item.breed"
+      class="p-6 text-center flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <img
+        class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://as2.ftcdn.net/v2/jpg/03/35/68/15/1000_F_335681545_kbojstRCD25Ld9eXgQ8KQbJoQv62x89Y.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {{ item.breed }}
+        </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          
+     <ul class="space-y-1 max-w-md list-none list-inside text-gray-500 dark:text-gray-400">
+    <li>
+       Country :{{ item.country }}
+    </li>
+    <li>
+         Coat: {{ item.coat }}
+    </li>
+    <li>
+       Origin:{{ item.origin }}
+    </li>
+    <li>Pattern:{{ item.pattern }}</li>
+</ul>
+        </p>
+      </div>
+    </div>
+  </div> -->
 </template>
+
+<script lang="ts" setup>
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+import { RouterLink } from "vue-router";
+
+const result = ref({});
+const res = ref<any>({});
+const search = ref("");
+
+//method for facts
+
+axios.get("https://catfact.ninja/fact").then((response) => {
+  console.log(response.data);
+  result.value = response.data;
+  localStorage.setItem("factSearch", JSON.stringify(result.value));
+});
+
+const catFactSearch = () => {
+  axios.get("https://catfact.ninja/fact").then((response) => {
+    console.log(response.data);
+    result.value = response.data;
+    localStorage.setItem("factSearch", JSON.stringify(response.data));
+    result.value = "";
+    result.value = JSON.parse(localStorage.getItem("factSearch"));
+  });
+};
+
+// method for breed names
+axios.get("https://catfact.ninja/breeds").then((response) => {
+  console.log(response.data);
+  res.value = response.data;
+  localStorage.setItem("fetchData", JSON.stringify(response.data));
+});
+
+const preLoad = async () => {
+  const response = await axios.get("https://catfact.ninja/breeds");
+  console.log(response.data);
+  res.value = await response.data;
+};
+
+const loadMore = async () => {
+  const response = await axios.get("https://catfact.ninja/breeds?page=2");
+  console.log(response.data);
+  res.value = await response.data;
+};
+
+const fetchEntry = () => {
+  return res.value.data.filter((item: any) =>
+    item.breed.toLowerCase().includes(search.value.toLowerCase())
+  );
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="p-10">
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div
+        class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+      >
+        <div
+          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="flex flex-wrap p-4 justify-center">
+            <img
+              src="https://www.somerzby.com.au/wp-content/uploads/2018/04/the-ultimate-list-of-cat-breeds-update-2018-800x800.jpg"
+              class="p-1 bg-white border rounded max-w-xs"
+              alt="..."
+            />
+          </div>
+          <div class="p-6 space-y-4 md:space-y-8 sm:p-8">
+            <h1
+              class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            >
+              Sign in to your account
+            </h1>
+            <form
+              @submit.prevent="handleLogin"
+              class="space-y-4 md:space-y-6"
+              action="#"
+            >
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Your email</label
+                >
+                <input
+                  type="email"
+                  v-model="form.email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  v-model="form.password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+
+              <button
+                type="submit"
+                class="w-full bg-blue-500 bg-primary-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+              <!-- <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?
+                <a
+                  href="#"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >Sign up</a
+                >
+              </p> -->
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, reactive, ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  name: "HomeView",
+  components: {},
+  setup() {
+    const router = useRouter();
+    const form = reactive({
+      email: "",
+      password: "" as string | number,
+
+      mockEmail: "admin@gmail.com",
+      mockPass: "2468" as string | number,
+    });
+    const handleLogin = () => {
+      console.log("You are successfully Logged in.");
+      if (form.email != form.mockEmail || form.password != form.mockPass) {
+        alert("Please enter valid email or password.");
+      } else {
+        router.replace({
+          name: "about",
+        });
+      }
+    };
+
+    return { form, handleLogin };
   },
 });
 </script>
